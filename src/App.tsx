92bb23e89070bd29015d84590763f33d60997041
zemosamurai <zemosamurai@gmail.com>
2022-11-04 22:39:31 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Counter} from "./Component/Counter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const minValue = 0
+    const maxValue = 5
+    const [count, setCount] = useState<number>(minValue)
+
+    return (
+        <div>
+            <Counter count={count} setCount={setCount} minValue={minValue} maxValue={maxValue}/>
+        </div>
+    );
 }
 
 export default App;
